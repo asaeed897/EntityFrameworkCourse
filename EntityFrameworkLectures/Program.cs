8cac844c667b7df190820560b25c14c7e4986937
awais <asaeed897>
2020-05-09 02:16:07 +0500

    One Issue Resolved and another came out.

---
+++ EntityFrameworkLectures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EntityFrameworkLectures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // 7. Database-first Workflow
+
+            // Now, I resolved that issue by installing EntityFrameworkCore instead of Simple EntityFramework 6
+            // because I am working in .NET Core project there that creating a problem.
+            // But Now I have some other problem. I couldn't find ADO.NET Model file... So I am working on it.
+            
+        }
+    }
+}
