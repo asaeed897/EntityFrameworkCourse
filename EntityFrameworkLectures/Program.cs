19d58b45ad42a3702336145e49a3f08292e7af7e
awais <asaeed897>
2020-05-09 02:36:05 +0500

    Resolved ADO.NET file issue

--- EntityFrameworkLectures/Program.cs
+++ EntityFrameworkLectures/Program.cs
@@ -9,9 +9,7 @@ namespace EntityFrameworkLectures
             Console.WriteLine("Hello World!");
             // 7. Database-first Workflow
 
-            // Now, I resolved that issue by installing EntityFrameworkCore instead of Simple EntityFramework 6
-            // because I am working in .NET Core project there that creating a problem.
-            // But Now I have some other problem. I couldn't find ADO.NET Model file... So I am working on it.
+            // Issue Resolved ADO.NET Entity Model file only appears in .NET Framework not in .NET Core
             
         }
     }
