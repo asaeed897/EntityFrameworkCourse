007643efea6c7480db82f946686c488ed110424a
awais <asaeed897>
2020-05-09 18:21:19 +0500

    Completed  Lesson 7 Database-First Workflow

--- CourseLectures/Program.cs
+++ CourseLectures/Program.cs
@@ -10,6 +10,17 @@ namespace CourseLectures
     {
         static void Main(string[] args)
         {
+            var context = new DatabaseFisrtDemoEntities1();
+            var post = new C0001_Post_Table()
+            {
+                Body = "Body",
+                DataPublished = DateTime.Now,
+                Title = "Title",
+                PostID = 1
+            };
+            context.C0001_Post_Table.Add(post);
+            context.SaveChanges();
+            // 7. Database-First Workflow
         }
     }
 }
