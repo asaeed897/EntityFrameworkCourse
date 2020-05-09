da59ea724444caec33700363a13f712facb453cc
awais <asaeed897>
2020-05-09 23:41:31 +0500

    Completed Lesson 11 and 12

--- CourseLectures/Program.cs
+++ CourseLectures/Program.cs
@@ -7,23 +7,12 @@ using System.Threading.Tasks;
 
 namespace CourseLectures
 {
-    public class Post
-    {
-        public int Id { get; set; }
-        public DateTime DatePublished { get; set; }
-        public string Title { get; set; }
-        public string Body { get; set; }
-    }
-    public class BlogDbContext : DbContext
-    {
-        public DbSet<Post> Posts { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             
-            // 10. Summary
+            // 11 and 12. Introduction &  Setting Up the Database
 
             // 
         }
