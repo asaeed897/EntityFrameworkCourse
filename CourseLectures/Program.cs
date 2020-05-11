9152a4b674f438ca90ae4930063a12f32c6c9772
awais <asaeed897>
2020-05-11 20:22:44 +0500

    Completed Lesson 19 Working with Enums

--- CourseLectures/Program.cs
+++ CourseLectures/Program.cs
@@ -7,22 +7,23 @@ using System.Threading.Tasks;
 
 namespace CourseLectures
 {
+    public enum Level : byte
+    {
+         Beginner = 1,
+         Intermediate = 2,
+         Advanced = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
-           
-            var dbContext = new PlutoDbContext();
-            var courses = dbContext.GetCourses();
-            
-            foreach (var course in courses)
-            {
-                Console.WriteLine(course.Title);
-            }
-            // 18. Function Imports
 
-            // Complex data types : Entity Frame use these to return data from database
-            // It become useful when we have to retrieve data from multiple tables or we want a subset of a data from large table
+            var course = new Course();
+            course.Level = Level.Beginner; // 1
+            // 19. Working with Enums
+
+            // We can create Enum in data model or in any other place in project but then we have to
+            // reference it.
         }
     }
 }
