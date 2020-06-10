174375782ea5fe57ab8a7d95d388b9bb398edc9a
awais <asaeed897@outlook.com>
2020-06-10 17:31:28 +0500

    Completed Lesson 61 Summary Section 7 Completed

--- CourseLectures/Program.cs
+++ CourseLectures/Program.cs
@@ -13,33 +13,7 @@ namespace CourseLectures
         {
             var context = new PlutoContext();
 
-            var author = context.Authors.Single(a=> a.Id== 5);
-
-            // MSDN way
-            context.Entry(author).Collection(a=> a.Courses).Load();  
-            // bad thing is that we can use it with single elements like author
-            // we can't use this with list of authors
-
-            // Mosh way
-            context.Courses.Where(c=> c.AuthorId == author.Id ).Load();    
-
-            foreach (var course in author.Courses)
-            {
-                Console.WriteLine("{0}", course.Name);
-            }
-
-            // In Mosh way we can use list of authors and etc
-            var authors = context.Authors.ToList();
-            var authorIds = authors.Select(a => a.Id);
-
-            context.Courses.Where(c=> authorIds.Contains(c.AuthorId) && c.FullPrice == 0).Load();
-
-            // 59 Explicit Loading
-
-            // It is same like eager loading but it have separate queries according to load
-            // it also have multiple round-trips to load objects.
-
-
+            // 61. Summary
 
         } 
     }
