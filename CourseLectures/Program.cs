ab594153c9ca7c6dc42354bdb56c0fdeed05a5f1
awais <asaeed897@outlook.com>
2020-06-11 00:38:01 +0500

    Completed Lesson 76 WPF Example

--- CourseLectures/Program.cs
+++ CourseLectures/Program.cs
@@ -12,22 +12,9 @@ namespace CourseLectures
     {
         static void Main(string[] args)
         {
-            using (var unitOfWork = new UnitOfWork(new PlutoContext()))
-            {
-                // Example 1
-                var course = unitOfWork.Courses.Get(1);
+            
 
-                // Example 2
-                var courses = unitOfWork.Courses.GetCoursesWithAuthors(1, 4);
-
-                // Example 3
-                var author = unitOfWork.Authors.GetAuthorWithCourses(5);
-                unitOfWork.Courses.RemoveRange(author.Courses);
-                unitOfWork.Authors.Remove(author);
-                unitOfWork.Complete();
-            }
-
-            // 75 ASP.NET MVC Example
+            // 76. WPF Example
 
         }
     }
