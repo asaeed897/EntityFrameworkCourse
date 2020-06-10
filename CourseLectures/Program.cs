4c43f37ff885ac14bd49d546e651ad8d836655c6
awais <asaeed897@outlook.com>
2020-06-10 19:36:00 +0500

    Completed Lesson 65 Updating Objects

--- CourseLectures/Program.cs
+++ CourseLectures/Program.cs
@@ -12,30 +12,13 @@ namespace CourseLectures
         static void Main(string[] args)
         {
             var context = new PlutoContext();
-            
-            // Two Approaches are common for adding object
 
-            // 1. Using an existing object in context
-            var authors = context.Authors.ToList();
-            var author = context.Authors.Single(a => a.Id == 2);
-
-            // there is 3rd approach also
-            context.Authors.Attach(author); // but its not recommended
-
-            var course = new Course
-            {
-                Name = "New Course 2",
-                Description = "New Description",
-                FullPrice = 19.95f,
-                Level = 1,
-                Author = author
-               // , AuthorId = 1 // 2. Using foreign key properties
-            };
-
-            context.Courses.Add(course);
+            var course = context.Courses.Find(4); // Single(c => c.Id == 4)
+            course.Name = "New Name";
+            course.AuthorId = 2;
 
             context.SaveChanges();
-            // 64. Adding Objects
+            // 65. Updating Objects
 
         } 
     }
